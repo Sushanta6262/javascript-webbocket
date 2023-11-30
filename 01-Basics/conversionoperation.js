048e725ced41c7c4d8b735501a3eb03916e0f7a0
Sushanta6262 <147519073+Sushanta6262@users.noreply.github.com>
2023-11-30 04:48:03 +0000

    file added

---
+++ 01-Basics/conversionoperation.js
@@ -0,0 +1,8 @@
+//let score =33;
+//let score = null;
+//let score ="gift";
+let score="33";
+let anotherIdNumber=Number(score);
+console.log(anotherIdNumber);
+console.log(typeof(score));
+console.log(2*3);
---
+++ 01-Basics/datatype.js
@@ -0,0 +1,6 @@
+let name="Sushant";
+let age=21;
+let isPresent=true;
+let id=Symbol("123");
+let anotherId=Symbol("123");
+console.log(id===anotherId);
