1db9345703d202a3e53c227ba671024eab27a3e2
Sushanta6262 <147519073+Sushanta6262@users.noreply.github.com>
2023-12-05 04:40:08 +0000

    file added

---
+++ 02-Intermediate/Array.js
@@ -0,0 +1,17 @@
+/*
+
+-Array is a object which can store similar type of data in a contigious memory location.
+-Array index start from 0.
+
+*/
+//const myArr=[3,4,"Sushant",5,6,7,8,9]
+//console.log(myArr[2]);
+const myArray=[1,2,3,4,5,6,7];
+console.log(myArray);
+myArray.unshift(0);
+console.log(myArray);
+myArray.shift();
+console.log(myArray);
+console.log(myArray.includes(9));
+console.log(myArray.indexOf(5));
+console.log(myArray);
