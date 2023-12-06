de171d8f914701dc8d9899d7c564d3fe38b3277b
Sushanta6262 <147519073+Sushanta6262@users.noreply.github.com>
2023-12-06 05:04:24 +0000

    file added

---
+++ 02-Intermediate/object.js
@@ -0,0 +1,32 @@
+// /*
+// -Object are super important for grouping the data and functionalities .So we can da a lot of things in Object of js.
+// -Object are constructet in key and value pair.
+// */
+// const jsUser={
+//     name:"Sushant",
+//     email:"sushant@gmail.com",
+//     age:22,
+//     location:"Odisha",
+//     isLoggedIn:"false",
+//     lastLogedInDays:["Monday","Wednessday","Friday"],
+// }
+// console.log(jsUser.name);
+// console.log(jsUser.email);
+// jsUser.email="webbocket@gmail.com";
+// Object.freeze(jsUser.email)
+// console.log(jsUser.email);
+// jsUser.email="gift@gmail.com";
+// console.log(jsUser.email);
+// jsUser.greeting=function(){
+//     console.log("Hello");
+// }
+// jsUser.greetingTwo=function(){
+//     console.log(`Hello js user ,${this.name}`);
+// }
+// jsUser.greeting();
+// jsUser.greetingTwo();
+// const tinderUser={}
+// tinderUser.id="1234",
+// tinderUser.emial="susha@gmail.com",
+// tinderUser.name="Baby"
+// console.log(tinderUser);
