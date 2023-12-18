ce56094b4e8f09efa2221f86799ce33c69cc4ec2
Sushanta6262 <147519073+Sushanta6262@users.noreply.github.com>
2023-12-18 05:26:35 +0000

    file added

--- Advance_js/promise.js
+++ Advance_js/promise.js
@@ -34,27 +34,46 @@
 // .catch(function(){
 //     console.log("Rejected the condition");
 // })
-var ans = new Promise(function(res,rej){
-    return res("reach the house");
+// var ans = new Promise(function(res,rej){
+//     return res("reach the house");
+// })
+// var p2 = ans.then(function(data){
+//     console.log(data);
+//     return new Promise(function(res,rej){
+//         return res("open the gate");
+//     })
+// })
+// var p3 = p2.then (function(data){
+//     console.log(data);
+//     return new Promise(function(res,rej){
+//         return res("prepare food and have that");
+//     })
+// })
+// var p4 = p3.then (function(data){
+//     console.log(data);
+//     return new Promise(function(res,rej){
+//         return res("scroll instagram reels");
+//     })
+// })
+// var p5 = p4.then (function(data){
+//     console.log(data);
+// })
+const promiseEx3 = new  Promise(function(res,rej){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            res({username: "pratush",password : "1234"})
+        }
+        else{
+            rej('Error: Somthing went wrong')
+        }
+    },2000)
 })
-var p2 = ans.then(function(data){
-    console.log(data);
-    return new Promise(function(res,rej){
-        return res("open the gate");
-    })
-})
-var p3 = p2.then (function(data){
-    console.log(data);
-    return new Promise(function(res,rej){
-        return res("prepare food and have that");
-    })
-})
-var p4 = p3.then (function(data){
-    console.log(data);
-    return new Promise(function(res,rej){
-        return res("scroll instagram reels");
-    })
-})
-var p5 = p4.then (function(data){
-    console.log(data);
+promiseEx3.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
 })
